79569a9b2850af3596ca93db202c1744ae3a12a6
wjl0115 <2861746361@qq.com>
2026-06-04 20:10:30 +0800

    版本2：新增键盘输入交互功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
+char* input() {
+    static char str[100];
+    printf("input:\n");
+    scanf("%s", str);
+    return str;
+}
+
+int main() {
+    char* Input = input(); 
+    printf("%s\n", Input);
     return 0;
 }
